3879cd7543af4125da387a10954a29d229aaff32
bauti <bautistamendibe10@gmail.com>
2025-03-29 19:25:26 -0300

    Se agrega el side bar con la libreria de shadcn

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,14 +1,14 @@
-import Header from "./Header";
-import Sidebar from "./Sidebar";
+import { AppSidebar } from "./AppSidebar";
+import {SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex h-screen flex-col">
-            <Header />
-            <div className="flex flex-1">
-                <Sidebar />
-                <main className="flex-1 p-6 overflow-auto">{children}</main>
-            </div>
-        </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <main>
+                <SidebarTrigger />
+                {children}
+            </main>
+        </SidebarProvider>
     );
 }
